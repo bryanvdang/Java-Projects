c9599ec0e931dedbb766633eec2a04e638c8cfe1
Bryan Dang <bryanvdang@gmail.com>
2017-07-08 03:57:28 -0700

    added more examples

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -14,8 +14,62 @@ public class Main {
         else{
             System.out.println("The value is not 1 or 2");
         }
+
+        //Switch Statement
+        //Good if you just want to test one variable
+        int switchValue = 3;
+
+        switch(switchValue){
+            case 1:
+                System.out.println("The value is 1");
+                break;
+            case 2:
+                System.out.println("The value is 2");
+                break;
+            case 3:case 4:case 5:
+                System.out.println("The value was either a 3, a 4, or a 5");
+                break;
+            default:
+                System.out.println("The value is not 1 or 2");
+                break;
+        }
+
+        String month = "JANUARY";
+        switch(month.toLowerCase()){
+            //^^ changes variable to lowercase.
+            case "january":
+                System.out.println("the string is january");
+                break;
+            case "june":
+                System.out.println("the string is june");
+                break;
+            default:
+                System.out.println("not sure");
+        }
+
+
+
+        char letter = 'a';
+
+        switch(letter){
+            case 'a':
+                System.out.println("the letter is 'a'");
+                break;
+            case 'b':
+                System.out.println("the letter is 'b'");
+                break;
+            case 'c':
+                System.out.println("the letter is 'c'");
+                break;
+            case 'd':
+                System.out.println("the letter is 'd'");
+            case 'e':
+                System.out.println("the letter is 'e'");
+                break;
+            default:
+                System.out.println("The letter is not either a, b, c, d, or e");
+                break;
+        }
+
     }
-    
-
-
 }
