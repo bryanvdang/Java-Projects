a7b0765867de41e0ca5728f1f6614a068c72fde6
Bryan Dang <bryanvdang@gmail.com>
2017-07-08 03:16:06 -0700

    switch practice

---
+++ Switch/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by bvdang on 7/8/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        int value = 1;
+
+        if (value == 1){
+            System.out.println("The value is 1");
+        }
+        else if(value == 2){
+            System.out.println("The value is 2");
+        }
+        else{
+            System.out.println("The value is not 1 or 2");
+        }
+    }
+    
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by bvdang on 7/6/2017.
+ */
+public class Main {
+}
